84c93c183ff46a82c1bf3d8dd9d9514d0c24b2d1
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-04-10 15:21:12 -0400

    Other Ways to Query the DOM

--- starter/index.js
+++ starter/index.js
@@ -14,11 +14,24 @@
 
 // const errors = document.querySelectorAll('.error');
 
-// console.log(errors);`
+// console.log(errors);
 
 
 /* Other Ways to Query the DOM */
 
+// get an element by ID
+// const title = document.getElementById('page-title');
+// console.log(title);
+
+// get elements by their class name 
+// const errors = document.getElementsByClassName('error');
+// console.log(errors); 
+// console.log(errors[0]);
+
+// get elements by their tag name 
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
 
 
 
