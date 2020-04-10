a18162d57203c256ab2c51245a878ca5eede0311
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-04-10 14:28:04 -0400

    The Query Selector

---
+++ starter/index.js
@@ -0,0 +1,35 @@
+/* The Query Selector */
+
+// console.log('Hello');
+// const para = document.querySelector('p');
+// console.log(para);
+
+// const paras = document.querySelectorAll('p');
+
+// paras.forEach(para => {
+//     console.log(para);
+// })
+
+// console.log(paras[2]); 
+
+// const errors = document.querySelectorAll('.error');
+
+// console.log(errors);`
+
+
+/* Other Ways to Query the DOM */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
