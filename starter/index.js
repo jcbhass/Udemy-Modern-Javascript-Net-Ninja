244cd10814c69e8a003c467e18f37e443f87dded
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-04-11 21:58:52 -0400

    Getting & Setting Attributes

--- starter/index.js
+++ starter/index.js
@@ -57,7 +57,15 @@ people.forEach(person => {
     content.innerHTML += `<p>${person}</p>`
 })
 
+const link = document.querySelector('a');
+console.log(link.getAttribute('href'));
+link.setAttribute('href', 'https://www.thenetninja.co.uk');
+link.innerText = 'The Net Ninja Website';
 
+const mssg = document.querySelector('p.error');
+console.log(mssg.getAttribute('class'));
+mssg.setAttribute('class', 'success');
+mssg.setAttribute('style', 'color: green;');
 
 
 
